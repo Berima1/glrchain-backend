f3b64bde2fa36e80587f63d3bac2e42cce360efb
Berima1 <Eddy199099@gmail.com>
2025-09-13 21:13:45 +0000

    Add files via upload

--- smart-contracts/EvidenceAnchor.sol
+++ smart-contracts/EvidenceAnchor.sol
@@ -1,1 +1,8 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract EvidenceAnchor {
+    event EvidenceAnchored(bytes32 indexed evidenceHash, string cid, address indexed reporter);
+    function anchorEvidence(bytes32 evidenceHash, string calldata cid) external {
+        emit EvidenceAnchored(evidenceHash, cid, msg.sender);
+    }
+}
