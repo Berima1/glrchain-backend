7d5c8aacd23da224cd9bc6454e2acefd61f72565
Berima1 <Eddy199099@gmail.com>
2025-09-18 04:43:07 +0000

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 import os
-from sqlalchemy.ext.asyncio import create_async_engine
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set")
-
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
