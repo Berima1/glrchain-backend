2e19de722712fac7b67f3a14228c60db4a5c7143
Berima1 <Eddy199099@gmail.com>
2025-09-19 03:39:52 +0000

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,18 @@
+# app/schemas.py
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class TokenData(BaseModel):
+    sub: Optional[str] = None
